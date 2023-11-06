d710d0e17a189a7380eb8276556beb1960366577
Erick <ericksjp703@gmail.com>
2023-11-06 18:25:39 -0300

    Transpilando os arquivos no webpack com o babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'transpiled', 'index.js'),
+    entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle[hash].js'
+        filename: 'bundle[fullhash].js',
+        clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html'),
             inject: 'body',
         }),
-        new CleanWebpackPlugin(),
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    }
 };
