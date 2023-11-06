238ed91c647d87e1cc1457350d361710e004f765
Erick <ericksjp703@gmail.com>
2023-11-06 19:06:09 -0300

    Criando primeiro componente React

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import App from "./App";
+
 ReactDOM.render(
-    <h1>Olá Mundo!!!!!!!</h1>,
+    <App />,
     document.querySelector("#root")
 )
